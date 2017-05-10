59bf98e7dc9fc4bc8b89dcfbfbd11248dec3571e
Dibyendu Das <dibyendud@capitalnumbers.com>
2017-05-10 11:11:25 +0530

    Initial setup with some implementation before 2 way binding

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue',
+        intro: 'Welcome to this tutorial <small>It is all about Vue.JS</small>',
+        isViewed: true,
+        todos: [{
+                text: 'Item 1',
+                id: 1
+                    },
+            {
+                text: 'Item 2',
+                id: 2
+                    },
+            {
+                text: 'Item 3',
+                id: 3
+                    },
+            {
+                text: 'Item 4',
+                id: 4
+                    },
+            {
+                text: 'Item 5',
+                id: 5
+                    },
+                ]
+    }
+});
